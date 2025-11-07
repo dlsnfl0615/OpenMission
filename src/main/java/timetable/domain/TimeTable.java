5f4a90d757702a01b63b9d951f12515a59101cf8
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-07 23:22:20 +0900

    refactor: 불필요 메소드 삭제

--- src/main/java/timetable/domain/TimeTable.java
+++ src/main/java/timetable/domain/TimeTable.java
@@ -1,22 +1,11 @@
 package timetable.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TimeTable {
-    private final List<Lecture> lectures = new ArrayList<>();
+    private final List<Lecture> lectures;
 
-    public void registerLecture(Lecture addLecture) {
-        lectures.add(addLecture);
-    }
-
-    public boolean exists(Lecture addLecture) {
-        for (Lecture lecture : lectures) {
-            if (lecture.overlapsWith(addLecture)) {
-                return true;
-            }
-        }
-
-        return false;
+    public TimeTable(List<Lecture> registeredLectures) {
+        lectures = registeredLectures;
     }
 }
