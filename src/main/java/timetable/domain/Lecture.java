e484557d59f6ca93e0dc745c993f85c87fc010c6
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-07 11:28:14 +0900

    refactor: 강의 분반에 대한 정보를 LectureSection 클래스로 정의

--- src/main/java/timetable/domain/Lecture.java
+++ src/main/java/timetable/domain/Lecture.java
@@ -4,14 +4,10 @@ import java.util.List;
 
 public class Lecture {
     private final String lectureName;
-    private final String professorName;
-    private final String lectureCode;
-    private final List<TimeSlot> lectureTimes;
+    private final List<LectureSection> lectureSections;
 
-    public Lecture(String lectureName, String professorName, String lectureCode, List<TimeSlot> lectureTimes) {
+    public Lecture(String lectureName, List<LectureSection> lectureSections) {
         this.lectureName = lectureName;
-        this.professorName = professorName;
-        this.lectureCode = lectureCode;
-        this.lectureTimes = lectureTimes;
+        this.lectureSections = lectureSections;
     }
 }
