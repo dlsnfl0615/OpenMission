1defbc8be9882f843f18defe9f40f852ed135336
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-07 23:21:40 +0900

    feat: 시간표 등록을 위한 강의 경로 탐색
    - 한 종류의 강의에 대해서 다음 종류의 강의를 하나씩 뽑아서 시간표 등록 강의 경로 탐색
    - 동일 종류 강의는 중복 신청할 수 없으므로 종류별로 강의 분할

--- src/main/java/timetable/domain/RegisterWizard.java
+++ src/main/java/timetable/domain/RegisterWizard.java
@@ -1,34 +1,63 @@
 package timetable.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RegisterWizard {
     private final List<TimeTable> timeTables;
-    private final List<Lecture> lectures;
-    private final int pickedLecturesCount;
+    private final List<Lecture> lectures; // 수강 신청하고자 하는 강의 목록
+    private final int uniqueLectures; // 강의 종류 수
 
-    public RegisterWizard(List<TimeTable> timeTables, List<Lecture> lectures) {
-        this.timeTables = timeTables;
+    public RegisterWizard(List<Lecture> lectures) {
+        this.timeTables = new ArrayList<>();
         this.lectures = lectures;
-    }
-
-    public int countLectureType() {
-        int count = 0;
-        List<String> lectureTypes = new ArrayList<>();
-
-        for (Lecture lecture : lectures) {
-
-        }
+        uniqueLectures = divideLectureByType().size();
     }
 
     public void makeTimeTable() {
-        TimeTable timeTable = new TimeTable();
+        List<Lecture> usedLectures = new ArrayList<>();
 
-        timeTables.add(timeTable);
+        dfsPickingLecture(usedLectures);
     }
 
-    public TimeTable pickLectures() {
+    public void dfsPickingLecture(List<Lecture> usedLectures) {
+        // 사용자가 원하는 강의 종류 수를 모두 신청하면 종료
+        if (usedLectures.size() == uniqueLectures) {
+            timeTables.add(new TimeTable(new ArrayList<>(usedLectures)));
+            return;
+        }
 
+        for (Lecture lecture : lectures) {
+            if (canAddLecture(usedLectures, lecture)) {
+                usedLectures.add(lecture);
+                dfsPickingLecture(usedLectures);
+                usedLectures.remove(usedLectures.size() - 1);
+            }
+        }
+
+    }
+
+    public boolean canAddLecture(List<Lecture> usedLectures, Lecture lecture) {
+        if (usedLectures.isEmpty()) {
+            return true;
+        }
+
+        for (Lecture usedLecture : usedLectures) {
+            if (usedLecture.overlapsTimeWith(lecture)
+                    || usedLecture.overlapsNameWith(lecture)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public Map<String, List<Lecture>> divideLectureByType() {
+        Map<String, List<Lecture>> map = new HashMap<>();
+
+        for (Lecture lecture : lectures) {
+            lecture.divide(map);
+        }
+
+        return map;
     }
 }
