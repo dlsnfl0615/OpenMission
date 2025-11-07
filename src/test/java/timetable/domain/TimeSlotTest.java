e5e842fa47ad8d9d3253e1354294791fd44120da
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-07 14:52:33 +0900

    test: TimeSlot에 대한 단위 테스트 작성

--- src/test/java/timetable/domain/TimeSlotTest.java
+++ src/test/java/timetable/domain/TimeSlotTest.java
@@ -13,8 +13,8 @@ class TimeSlotTest {
         TimeSlot timeSlot2 = new TimeSlot(Day.TUE, LocalTime.of(13,0,0), LocalTime.of(14,30,0));
         TimeSlot timeSlot3 = new TimeSlot(Day.MON, LocalTime.of(14,30,0), LocalTime.of(15,30,0));
 
-        assertThat(timeSlot1.doesOverlap(timeSlot2)).isEqualTo(false);
-        assertThat(timeSlot1.doesOverlap(timeSlot3)).isEqualTo(false);
+        assertThat(timeSlot1.overlapsWith(timeSlot2)).isEqualTo(false);
+        assertThat(timeSlot1.overlapsWith(timeSlot3)).isEqualTo(false);
     }
 
     @Test
@@ -22,6 +22,6 @@ class TimeSlotTest {
         TimeSlot timeSlot1 = new TimeSlot(Day.MON, LocalTime.of(13,0,0), LocalTime.of(14,30,0));
         TimeSlot timeSlot2 = new TimeSlot(Day.MON, LocalTime.of(13,30,0), LocalTime.of(15,0,0));
 
-        assertThat(timeSlot1.doesOverlap(timeSlot2)).isEqualTo(true);
+        assertThat(timeSlot1.overlapsWith(timeSlot2)).isEqualTo(true);
     }
 }
