3057f98d975c6be381d2df5defd58a620d9cba29
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-07 14:50:57 +0900

    refactor: 분반을 하나의 강의 단위로 수정

--- src/main/java/timetable/domain/Lecture.java
+++ src/main/java/timetable/domain/Lecture.java
@@ -4,10 +4,40 @@ import java.util.List;
 
 public class Lecture {
     private final String lectureName;
-    private final List<LectureSection> lectureSections;
+    private final String professorName;
+    private final String lectureCode;
+    private final List<TimeSlot> lectureTimes;
 
-    public Lecture(String lectureName, List<LectureSection> lectureSections) {
+    public Lecture(String lectureName, String professorName, String lectureCode, List<TimeSlot> lectureTimes) {
         this.lectureName = lectureName;
-        this.lectureSections = lectureSections;
+        this.professorName = professorName;
+        this.lectureCode = lectureCode;
+        this.lectureTimes = lectureTimes;
+    }
+
+    public boolean overlapsWith(Lecture otherLecture) {
+        // 동일 교과목은 수강할 수 없음
+        if (this.lectureName.equals(otherLecture.lectureName)
+                || this.lectureCode.equals(otherLecture.lectureCode)) {
+            return true;
+        }
+
+        for (TimeSlot timeSlot : this.lectureTimes) {
+            if (hasConflictWith(timeSlot, otherLecture.lectureTimes)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasConflictWith(TimeSlot thisTimeSlot, List<TimeSlot> otherLectureTimes) {
+        for (TimeSlot otherTimeSlot : otherLectureTimes) {
+            if (thisTimeSlot.overlapsWith(otherTimeSlot)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
