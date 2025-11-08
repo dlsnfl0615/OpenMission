8110233763ca9ddc8bcc12b54863428a3d8241a0
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-08 17:44:10 +0900

    test: 테스트 코드를 위한 강의 정보 정의

---
+++ src/test/java/timetable/lectures/TestLectureDatas.java
@@ -0,0 +1,64 @@
+package timetable.lectures;
+
+import timetable.domain.Day;
+import timetable.domain.Lecture;
+import timetable.domain.TimeSlot;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class TestLectureDatas {
+    // 자바, 월 08:30~10:00
+    public static final List<TimeSlot> lectureTimes1 =
+            List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture1 =
+            new Lecture("자바", "유재석", "CSC-1", lectureTimes1);
+
+    // 자바, 화 08:30~10:00
+    public static final List<TimeSlot> lectureTimes2 =
+            List.of(new TimeSlot(Day.TUE, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture2 =
+            new Lecture("자바", "박명수", "CSC-2", lectureTimes2);
+
+    // c++, 수 08:30~10:00
+    public static final List<TimeSlot> lectureTimes3 =
+            List.of(new TimeSlot(Day.WED, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture3 =
+            new Lecture("C++", "정준하", "CSC-3", lectureTimes3);
+
+    // c++, 목 08:30~10:00
+    public static final List<TimeSlot> lectureTimes4 =
+            List.of(new TimeSlot(Day.THU, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture4 =
+            new Lecture("C++", "정형돈", "CSC-4", lectureTimes4);
+
+    // 파이썬, 월 09:00~10:00 (lecture1과 겹침)
+    public static final List<TimeSlot> lectureTimes5 =
+            List.of(new TimeSlot(Day.MON, LocalTime.of(9, 0), LocalTime.of(10, 0)));
+    public static final Lecture lecture5 =
+            new Lecture("파이썬", "길", "CSC-5", lectureTimes5);
+
+    // 파이썬, 화 11:00~12:00
+    public static final List<TimeSlot> lectureTimes6 =
+            List.of(new TimeSlot(Day.TUE, LocalTime.of(11, 0), LocalTime.of(12, 0)));
+    public static final Lecture lecture6 =
+            new Lecture("파이썬", "노홍철", "CSC-6", lectureTimes6);
+
+    // 파이썬, 금 08:30~10:00
+    public static final List<TimeSlot> lectureTimes7 =
+            List.of(new TimeSlot(Day.FRI, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture7 =
+            new Lecture("파이썬", "하하", "CSC-7", lectureTimes7);
+
+    // 알고리즘, 월 08:30~10:00
+    public static final List<TimeSlot> lectureTimes8 =
+            List.of(new TimeSlot(Day.MON, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture8 =
+            new Lecture("알고리즘", "유재석", "CSC-1", lectureTimes8);
+
+    // 알고리즘, 화 08:30~10:00
+    public static final List<TimeSlot> lectureTimes9 =
+            List.of(new TimeSlot(Day.TUE, LocalTime.of(8, 30), LocalTime.of(10, 0)));
+    public static final Lecture lecture9 =
+            new Lecture("알고리즘", "박명수", "CSC-2", lectureTimes9);
+}
