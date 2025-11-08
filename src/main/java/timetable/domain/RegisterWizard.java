358f055322f205c66c2260c940be824ab7319a42
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-08 17:41:09 +0900

    feat: 객체가 상태를 가지지 않도록 수정

--- src/main/java/timetable/domain/RegisterWizard.java
+++ src/main/java/timetable/domain/RegisterWizard.java
@@ -3,34 +3,32 @@ package timetable.domain;
 import java.util.*;
 
 public class RegisterWizard {
-    private final List<TimeTable> timeTables;
-    private final List<Lecture> lectures; // 수강 신청하고자 하는 강의 목록
-    private final int uniqueLectures; // 강의 종류 수
-
-    public RegisterWizard(List<Lecture> lectures) {
-        this.timeTables = new ArrayList<>();
-        this.lectures = lectures;
-        uniqueLectures = divideLectureByType().size();
-    }
-
-    public void makeTimeTable() {
+    public List<TimeTable> makeTimeTable(List<Lecture> lecturesToRegister) {
+        int uniqueLectures = divideLectureByType(lecturesToRegister).size(); // 강의 종류 수
         List<Lecture> usedLectures = new ArrayList<>();
+        List<TimeTable> timeTables = new ArrayList<>();
 
-        dfsPickingLecture(usedLectures);
+        dfsPickingLecture(lecturesToRegister, usedLectures, timeTables, uniqueLectures);
+
+        return timeTables;
     }
 
-    public void dfsPickingLecture(List<Lecture> usedLectures) {
+    // 조건에 맞는 모든 강의 시간표 찾기
+    public void dfsPickingLecture(List<Lecture> lecturesToRegister,
+                                  List<Lecture> usedLectures,
+                                  List<TimeTable> timeTables,
+                                  int uniqueLectures) {
         // 사용자가 원하는 강의 종류 수를 모두 신청하면 종료
         if (usedLectures.size() == uniqueLectures) {
             timeTables.add(new TimeTable(new ArrayList<>(usedLectures)));
             return;
         }
 
-        for (Lecture lecture : lectures) {
+        for (Lecture lecture : lecturesToRegister) {
             if (canAddLecture(usedLectures, lecture)) {
                 usedLectures.add(lecture);
-                dfsPickingLecture(usedLectures);
-                usedLectures.remove(usedLectures.size() - 1);
+                dfsPickingLecture(lecturesToRegister, usedLectures, timeTables, uniqueLectures);
+                usedLectures.removeLast();
             }
         }
 
@@ -51,10 +49,12 @@ public class RegisterWizard {
         return true;
     }
 
-    public Map<String, List<Lecture>> divideLectureByType() {
+    // 동일 종류 강의(이름이 동일한 강의)는 중복 수강신청할 수 없음.
+    // 강의명을 key로 정의 -> 분반을 하나의 강의로 합쳐 강의 종류 수 셀 수 있음.
+    private Map<String, List<Lecture>> divideLectureByType(List<Lecture> lecturesToRegister) {
         Map<String, List<Lecture>> map = new HashMap<>();
 
-        for (Lecture lecture : lectures) {
+        for (Lecture lecture : lecturesToRegister) {
             lecture.divide(map);
         }
 
