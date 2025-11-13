beebd9a5fc728d1b4f01cb3e933695b0aaab8d1e
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-13 22:16:19 +0900

    feat: 공강날 지정하는 로직 추가를 위해 인자 추가 전달

--- src/main/java/timetable/service/TimeTableMaker.java
+++ src/main/java/timetable/service/TimeTableMaker.java
@@ -4,6 +4,7 @@ import timetable.domain.Lecture;
 import timetable.domain.LectureMatcher;
 import timetable.domain.RegisterWizard;
 import timetable.domain.TimeTable;
+import timetable.domain.Day;
 
 import java.util.List;
 
@@ -16,8 +17,8 @@ public class TimeTableMaker {
         this.lectureMatcher = lectureMatcher;
     }
 
-    public List<TimeTable> makeTimeTable(List<String> lectureCodes, List<Lecture> availableLectures) {
+    public List<TimeTable> makeTimeTable(List<String> lectureCodes, List<Lecture> availableLectures, List<Day> noLectureDays) {
         List<Lecture> lecturesToRegister = lectureMatcher.findLecturesWithCode(lectureCodes, availableLectures);
-        return registerWizard.makeTimeTable(lecturesToRegister);
+        return registerWizard.makeTimeTable(lecturesToRegister, noLectureDays);
     }
 }
