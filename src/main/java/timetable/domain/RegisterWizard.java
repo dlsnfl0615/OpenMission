40e32c1f67d00fa684cd864423efddafbb46729f
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-13 22:12:41 +0900

    feat: 공강날 지정하는 로직 추가

--- src/main/java/timetable/domain/RegisterWizard.java
+++ src/main/java/timetable/domain/RegisterWizard.java
@@ -3,20 +3,22 @@ package timetable.domain;
 import java.util.*;
 
 public class RegisterWizard {
-    public List<TimeTable> makeTimeTable(List<Lecture> lecturesToRegister) {
+    List<Lecture> usedLectures;
+
+    public List<TimeTable> makeTimeTable(List<Lecture> lecturesToRegister, List<Day> noLectureDays) {
+        usedLectures = new ArrayList<>();
         int uniqueLectures = divideLectureByType(lecturesToRegister).size(); // 강의 종류 수
-        List<Lecture> usedLectures = new ArrayList<>();
         List<TimeTable> timeTables = new ArrayList<>();
 
-        dfsPickingLecture(lecturesToRegister, usedLectures, timeTables, uniqueLectures);
+        dfsPickingLecture(lecturesToRegister, timeTables, noLectureDays, uniqueLectures);
 
         return timeTables;
     }
 
     // 조건에 맞는 모든 강의 시간표 찾기
     public void dfsPickingLecture(List<Lecture> lecturesToRegister,
-                                  List<Lecture> usedLectures,
                                   List<TimeTable> timeTables,
+                                  List<Day> noLectureDays,
                                   int uniqueLectures) {
         // 사용자가 원하는 강의 종류 수를 모두 신청하면 종료
         if (usedLectures.size() == uniqueLectures) {
@@ -25,16 +27,20 @@ public class RegisterWizard {
         }
 
         for (Lecture lecture : lecturesToRegister) {
-            if (canAddLecture(usedLectures, lecture)) {
+            if (canAddLecture(lecture, noLectureDays)) {
                 usedLectures.add(lecture);
-                dfsPickingLecture(lecturesToRegister, usedLectures, timeTables, uniqueLectures);
+                dfsPickingLecture(lecturesToRegister, timeTables, noLectureDays, uniqueLectures);
                 usedLectures.removeLast();
             }
         }
 
     }
 
-    public boolean canAddLecture(List<Lecture> usedLectures, Lecture lecture) {
+    public boolean canAddLecture(Lecture lecture, List<Day> noLectureDays) {
+        if (lecture.conflictsWithNoLectureDays(noLectureDays)) {
+            return false;
+        }
+
         if (usedLectures.isEmpty()) {
             return true;
         }
@@ -50,7 +56,7 @@ public class RegisterWizard {
     }
 
     // 동일 종류 강의(이름이 동일한 강의)는 중복 수강신청할 수 없음.
-    // 강의명을 key로 정의 -> 분반을 하나의 강의로 합쳐 강의 종류 수 셀 수 있음.
+    // 강의명을 key로 정의하여 분반을 하나의 강의로 합쳐 강의 종류 수 셀 수 있음.
     private Map<String, List<Lecture>> divideLectureByType(List<Lecture> lecturesToRegister) {
         Map<String, List<Lecture>> map = new HashMap<>();
 
