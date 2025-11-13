bf51d9f3c3f38a6a4021124dfd83e6764e923f5e
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-13 23:30:17 +0900

    feat: 사용자로부터 입력받은 강의와 요일을 쉼표로 분할

---
+++ src/main/java/timetable/util/InputParser.java
@@ -0,0 +1,34 @@
+package timetable.util;
+
+import timetable.domain.Day;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class InputParser {
+    private static final Map<String, Day> dayMap = Map.of(
+            "월", Day.MON,
+            "화", Day.TUE,
+            "수", Day.WED,
+            "목", Day.THU,
+            "금", Day.FRI
+    );
+
+    private InputParser() {
+
+    }
+
+    public static List<String> separateInput(String lectures) {
+        return Arrays.stream(lectures.split(",")).toList();
+    }
+
+    // 한글로 월, 화, 수, ... 로 입력된 문자열을 Day 자료형으로 변환
+    public static List<Day> convertToDay(List<String> koreanDays) {
+        return koreanDays.stream()
+                .map(koreanDay -> {
+                    return Day.getDay(koreanDay);
+                })
+                .toList();
+    }
+}
