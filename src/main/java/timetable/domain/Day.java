fa9556d7b62e2083223c1cbdc97380d1929a584f
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-13 23:29:48 +0900

    feat: 한글 요일 추가

--- src/main/java/timetable/domain/Day.java
+++ src/main/java/timetable/domain/Day.java
@@ -1,5 +1,25 @@
 package timetable.domain;
 
 public enum Day {
-    MON, TUE, WED, THU, FRI
+    MON("월"),
+    TUE("화"),
+    WED("수"),
+    THU("목"),
+    FRI("금");
+    
+    private final String koreanDay;
+    
+    Day(String koreanDay) {
+        this.koreanDay = koreanDay;
+    }
+    
+    public static Day getDay(String koreanDay) {
+        for (Day day : Day.values()) {
+            if (day.koreanDay.equals(koreanDay)) {
+                return day;
+            }
+        }
+
+        return null;
+    }
 }
